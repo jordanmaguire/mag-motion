d19cf1c139a4bbca6bf87d6f44b218c2c75e9240
Jordan Maguire <jmaguire@thefrontiergroup.com.au>
2014-07-29 09:31:33 +0800

    Bug Fix: updateFrame no longer updates frame twice, which caused animation issues

--- lib/mag-motion/cocoa-touch/ui_view.rb
+++ lib/mag-motion/cocoa-touch/ui_view.rb
@@ -55,24 +55,14 @@ class UIView
 
   # Terse syntax for updating the origin and size of a UIView
   #
-  # options is a hash with the same keys as updateOrigin and updateSize
-  def updateFrame(options)
-    updateOrigin(options)
-    updateSize(options)
-    frame
-  end
-
-private
-
-  # Terse syntax for updating just the origin of a UIView
-  #
   # options is a hash with the following available keys:
   #   * adjustWidth: A boolean indicating whether or not the width should adjust to cover
   #                  the same space it did before the change. Ignored if false or if x is nil
   #   * x: A value to change the x origin to
   #   * y: A value to change the y origin to
-  def updateOrigin(options)
-    return frame if options.nil? || (options[:x].nil? && options[:y].nil?)
+  #   * width: A value to change the width to
+  #   * height: A value to change the height to
+  def updateFrame(options)
     self.frame = frame.tap do |f|
       # If we pass the adjustWidth option through, we must ensure that the total width of the
       # view remains the same.
@@ -84,17 +74,6 @@ private
 
       f.origin.x = options[:x] unless options[:x].nil?
       f.origin.y = options[:y] unless options[:y].nil?
-    end
-  end
-
-  # Terse syntax for updating just the size of a UIView
-  #
-  # options is a hash with the following available keys:
-  #   * width: A value to change the width to
-  #   * height: A value to change the height to
-  def updateSize(options)
-    return frame if options.nil? || (options[:width].nil? && options[:height].nil?)
-    self.frame = frame.tap do |f|
       f.size.width = options[:width] unless options[:width].nil?
       f.size.height = options[:height] unless options[:height].nil?
     end
